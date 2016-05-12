cdc9e4631a4a9dedff33937748d2eea7501c5791
shem-sergey <sergey.shemyakov@duallab.com>
2016-05-12 21:38:28 +0300

    keyListener improved

--- Scripts/keyListener.sh
+++ Scripts/keyListener.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
 #Change layout anyway
-LAYOUT=$(setxkbmap -print | grep xkb_symbols | awk '{print $4}' | awk -F"+" '{print $2}')
-echo $LAYOUT > /home/robaut/yay.txt
-if [ "$LAYOUT" == "us" ];
+LAYOUT=$(gsettings get org.gnome.desktop.input-sources current | awk '{print $2}')
+
+((LAYOUT++))
+
+if [ $LAYOUT -eq 3 ];
 	then
-		setxkbmap by
-		echo by > /home/robaut/yay.txt
-
-
-else if [ "$LAYOUT" == "by" ];
-	then
-		setxkbmap ru
-		echo ru > /home/robaut/yay.txt
-		
-
-		else if [ "$LAYOUT" == "ru" ];
-			then
-				setxkbmap us
-				echo us > /home/robaut/yay.txt
-			fi
-	fi
+		LAYOUT=0
 fi
+
 #Change it back if in Idea
 ACTIVE=$(xprop -id `xdotool getwindowfocus` | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
 
-IDEA=$(ps -ef | awk '$8=="/home/robaut/Installed/idea-IC-145.258.11/bin/../jre/jre/bin/java" {print $2}')
+IDEA=$(ps -ef | awk '$8=="/home/shemyakovsergey/Installed/idea-IC-145.597.3/bin/../jre/jre/bin/java" {print $2}')
 
-if [ $ACTIVE==$IDEA ];
+if [ $ACTIVE!=$IDEA ];
 	then
-		setxkbmap us
+		gsettings set org.gnome.desktop.input-sources current $LAYOUT 
 fi
-
-	
